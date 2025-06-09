6aff1ee6918fe209464363cf6e050dd00a151844
Shaurya Patel <shauryapatel1995@gmail.com>
2025-06-09 09:34:57 -0700

    Adding present bit

--- mm/memory.c
+++ mm/memory.c
@@ -3711,9 +3711,10 @@ vm_fault_t do_swap_page_collect(struct vm_fault *vmf, struct pt_regs *regs)
         // int i, increment = (sizeof(long)) * 8, total_num = PAGE_SIZE / sizeof(long);
         int i = 0;
         for(i = 0; i < 512; i++, p++) {
-            pge_t * pte = lookup_address_in_pgd(curr->mm->pgd, *p, &level);
+            pte_t * pte = lookup_address_in_pgd(curr->mm->pgd, *p, &level);
             if (pte)
-                printk(KERN_CRIT "Loc: %d, val: %llx\n",i,*p); 
+                printk(KERN_CRIT "Loc: %d, val: %llx, present:%d\n",
+                        i,*p, pte_present(pte)); 
         }
     }
 unlock:
