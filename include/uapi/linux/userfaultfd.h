554c78aae3af468e4438c1285efe412c44f9e75b
Shaurya Patel <shauryapatel1995@gmail.com>
2025-09-17 14:40:07 -0700

    Changing struct definition to adhere to size constraints

--- include/uapi/linux/userfaultfd.h
+++ include/uapi/linux/userfaultfd.h
@@ -89,15 +89,15 @@ struct uffd_msg {
 
 	__u8	reserved1;
 	__u16	reserved2;
-	__u32	reserved3;
+	//__u32	reserved3;
 
 	union {
 		struct {
 			__u64	flags;
 			__u64	address;
-            __u64   pc;
+            		__u64   pc;
 			union {
-				__u32 ptid;
+				//__u32 ptid;
 			} feat;
 		} pagefault;
 
